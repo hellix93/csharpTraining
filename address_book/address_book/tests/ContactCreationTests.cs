4c880ff93d84eede6c0db103ffe6d0c1b6698cd9
hellix93 <maxmix@inbox.ru>
2019-09-15 10:07:41 +0300

    Задание №15: реализовать загрузку тестовых данных из файла

--- address_book/address_book/tests/ContactCreationTests.cs
+++ address_book/address_book/tests/ContactCreationTests.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace WebAddressbookTests
 {
@@ -27,7 +31,22 @@ namespace WebAddressbookTests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            List<ContactData> groups = new List<ContactData>();
+
+            return (List<ContactData>)
+                 new XmlSerializer(typeof(List<ContactData>))
+                 .Deserialize(new StreamReader(@"contacts.xml"));
+
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromXmlFile")]
         public void ContactCreationTest(ContactData contact)
         {
             /*
