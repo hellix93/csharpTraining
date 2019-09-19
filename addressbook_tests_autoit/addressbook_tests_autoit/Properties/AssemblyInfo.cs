5cdce21c12f35f850af86b1da9fb74faf8cada48
hellix93 <maxmix@inbox.ru>
2019-09-19 23:57:03 +0300

    Задание №18: Реализовать тесты для добавления и удаления групп

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fa27b8f-b66b-4638-8e32-9e29408ee027")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupDeleteTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupDeleteTests : TestBase
+    {
+        [Test]
+        public void TestGroupDelete()
+        {
+            GroupData newGroupForDelete = new GroupData()
+            {
+                Name = "deleteTest"
+            };
+            app.Groups.Add(newGroupForDelete);
+
+            List<GroupData> groupsBeforeDelete = app.Groups.GetGroupList();
+
+            app.Groups.Delete();
+
+            List<GroupData> groupsAfterDelete = app.Groups.GetGroupList();
+            groupsAfterDelete.Add(newGroupForDelete);
+            groupsBeforeDelete.Sort();
+            groupsAfterDelete.Sort();
+
+            Assert.AreEqual(groupsBeforeDelete, groupsAfterDelete);
+        }
+    }
+}
